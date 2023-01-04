7ea936288283c410e71ec2180bac6addcb6154fc
dmoore11772 <121840750+dmoore11772@users.noreply.github.com>
2023-01-03 22:22:40 -0600

    schedule pull js

---
+++ nba-schedule-pull.js
@@ -0,0 +1,23 @@
+fetch('https://www.example.com/nba-schedule')
+  .then(response => response.json())
+  .then(schedule => {
+    for (const game of schedule) {
+        const dateCell = document.createElement('td');
+        dateCell.textContent = game.date;
+        const matchupCell = document.createElement('td');
+        matchupCell.textContent = `${game.awayTeam} @ ${game.homeTeam}`;
+        const resultCell = document.createElement('td');
+        resultCell.textContent = game.result;
+        const timeCell = document.createElement('td');
+        timeCell.textContent = game.time;
+        const tvCell = document.createElement('td');
+        tvCell.textContent = game.tv;
+        const row = document.createElement('tr');
+        row.appendChild(dateCell);
+        row.appendChild(matchupCell);
+        row.appendChild(resultCell);
+        row.appendChild(timeCell);
+        row.appendChild(tvCell);
+        document.getElementById('schedule-table').appendChild(row);
+      }      
+  });
